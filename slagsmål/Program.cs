b4177924fad5ddd9f6c81cea8f46ad80555118ac
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-09-26 11:09:23 +0200

    gsdsdg

--- slagsmål/Program.cs
+++ slagsmål/Program.cs
@@ -2,9 +2,14 @@
 Player player2 = new Player();
 var random = new Random();
 
-// object [][] weapon = {
-// new object[] {"hand", 10, 30, 100},
-// };
+object [][] weapon = {
+new object[] {"hand", 10, 30, 100},
+new object[] {"knä", 5,40,80}
+};
+
+for(int i=0;i<weapon.Count() ;i++ ){
+    System.Console.WriteLine($"{i+1}: {weapon[i][0]}  {weapon[i][1]}-{weapon[i][2]} damahe  {weapon[i][3]}% chanse to hit");
+}
 
 System.Console.WriteLine("vad heter du");
 player1.name = Console.ReadLine();
@@ -25,21 +30,32 @@ while (player1.hp > 0 && player2.hp > 0)
 
     int damage = random.Next(20);
     player1.hp -= damage;
-    System.Console.WriteLine($" {player1.name} gör {damage} skada");
+    System.Console.WriteLine($"{player1.name} gör {damage} skada");
 
     //player2 slår
 
     damage = random.Next(20);
     player2.hp -= damage;
-    System.Console.WriteLine($" {player2.name} gör {damage} skada");
+    System.Console.WriteLine($"{player2.name} gör {damage} skada");
     Console.ReadLine();
 
 
-    System.Console.WriteLine($"your remaining hp is {player1.hp} {player2.name}s hp is {player2.hp}");
+    System.Console.Write($"your remaining hp is ");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write(player1.hp);
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write($"\n{player2.name}s hp is ");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(player2.hp);
+    Console.ForegroundColor = ConsoleColor.White;
 
 }
 //Slut på strid
-if(player1.hp <= 0 || player2.hp <= 0){
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+if (player1.hp <= 0 || player2.hp <= 0)
+{
     System.Console.WriteLine($"striden är över");
 }
 
